5a6b1e95fe902595257c4b020d5aa7462df08a44
Andreas Rissling <a.rissling@steinberg.de>
2022-02-10 09:03:42 +0100

    Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'react-app'],
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: `react-app`,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
