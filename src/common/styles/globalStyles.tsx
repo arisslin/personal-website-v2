113e462e837d9a72e223c891e2fea7fdcd722c11
Andreas Rissling <a.rissling@steinberg.de>
2022-03-02 11:30:04 +0100

    Install global font open-sans

--- src/common/styles/globalStyles.tsx
+++ src/common/styles/globalStyles.tsx
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import '@fontsource/open-sans';
 
 const GlobalStyles = createGlobalStyle`
   :root {
     // Colors
     --color-dark: #131b20;
     --color-light: #fafafa;
+
+    // Typo
+    --font-family-global: 'Open Sans', Helvetica, sans-serif;
   }
 
   * {
@@ -14,6 +18,11 @@ const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     background-color: #fafafa;
+    font-family: var(--font-family-global);
+  }
+
+  p {
+    font-size: 1rem;
   }
 `;
 
