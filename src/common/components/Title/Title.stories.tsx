346e5098530cdaceb84b179703bafa35aacae53a
Andreas Rissling <a.rissling@steinberg.de>
2022-03-17 10:17:15 +0100

    Add description to Title story

--- src/common/components/Title/Title.stories.tsx
+++ src/common/components/Title/Title.stories.tsx
@@ -5,6 +5,25 @@ import TitleComponent from './Title';
 export default {
   title: 'Components/Title',
   component: TitleComponent,
+  parameters: {
+    docs: {
+      description: {
+        component: getDescription(),
+      },
+    },
+  },
 } as Meta;
+
+function getDescription(): string {
+  return (
+    '<h2>About</h2>' +
+    '<p>' +
+    'The <b>Title</b> component got developed in March 2022. It shows the' +
+    'title of the entire website. It can be used on every page of the' +
+    "website but it's mainly but it was designed to be displayed on the " +
+    'home page.' +
+    '</p>'
+  );
+}
 
 export const Title: Story = () => <TitleComponent />;
