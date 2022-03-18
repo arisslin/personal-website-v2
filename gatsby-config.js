a374809a404489111a78bed6b5fd96a8818785a8
Andreas Rissling <a.rissling@steinberg.de>
2022-03-18 12:06:22 +0100

    Add meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'Andreas Rissling',
+    siteUrl: `https://www.gueteklasse-a.de`,
   },
   plugins: [
     'gatsby-plugin-styled-components',
