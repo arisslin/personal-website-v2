1781761c1bc444f14ad2cd66c6146444a9809fca
Andreas Rissling <a.rissling@steinberg.de>
2022-05-16 10:22:57 +0200

    Add typosystem

--- src/common/styles/typography.tsx
+++ src/common/styles/typography.tsx
@@ -3,11 +3,22 @@ import '@fontsource/open-sans/300.css';
 import '@fontsource/open-sans/400.css';
 import '@fontsource/open-sans/700.css';
 
+const fontSizeInPx = 16;
+const rootFontSizeInRem = 1;
+
+export const fontSizesInRem = {
+  root: rootFontSizeInRem,
+  base: 17 / fontSizeInPx,
+  large: 18 / fontSizeInPx,
+  headingS: 20 / fontSizeInPx,
+  headingM: 24 / fontSizeInPx,
+  headingL: 28 / fontSizeInPx,
+  headingXl: 32 / fontSizeInPx,
+} as const;
+
 const GlobalTypography = createGlobalStyle`
   :root {
-    font-size: 16px;
-
-    --font-size-base: 1rem;
+    font-size: ${fontSizeInPx}px;
 
     --font-family-global: 'Open Sans', Helvetica, sans-serif;
   }`;
