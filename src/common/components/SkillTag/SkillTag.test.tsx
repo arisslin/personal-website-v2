688ff8bf75863a9e3ba3092e7f473b8deb7b5cfc
Andreas Rissling <a.rissling@steinberg.de>
2022-07-08 18:09:09 +0200

    Add snapshot test for SkillTag

--- src/common/components/SkillTag/SkillTag.test.tsx
+++ src/common/components/SkillTag/SkillTag.test.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import SkillTag, { testId } from './SkillTag';
+import { doSnapshotTest } from '../../helper/testHelper';
 
 describe('SkillTag', () => {
-  it('renders with text', () => {
-    const text = 'TypeScript';
-    const component = <SkillTag text={text} />;
+  const text = 'TypeScript';
+  const component = <SkillTag text={text} />;
 
+  it('renders with text', () => {
     render(component);
 
     expect(screen.getByText(text)).toBeInTheDocument();
   });
 
   it('does not render if text is an empty string', () => {
-    const component = <SkillTag text='' />;
-
-    render(component);
+    render(<SkillTag text='' />);
 
     expect(screen.queryByTestId(testId)).not.toBeInTheDocument();
   });
+
+  doSnapshotTest(component);
 });
