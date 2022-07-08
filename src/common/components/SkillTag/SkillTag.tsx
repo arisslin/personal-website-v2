4c1b822ba8e4611d2ab86c90c6094c39516d5322
Andreas Rissling <a.rissling@steinberg.de>
2022-07-08 11:25:32 +0200

    Add basic style to SkillTag

--- src/common/components/SkillTag/SkillTag.tsx
+++ src/common/components/SkillTag/SkillTag.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import styled from 'styled-components/macro';
 
 type SkillTagProps = {
   text: string;
 };
 
-const SkillTag = ({ text }: SkillTagProps) => <span>{text}</span>;
+const SkillTag = ({ text }: SkillTagProps) => (
+  <SkillTagStyled>{text}</SkillTagStyled>
+);
+
+const SkillTagStyled = styled.span`
+  background-color: var(--color-yo);
+  border-radius: 3px;
+  color: var(--color-light);
+  padding: 10px 12px;
+`;
 
 export default SkillTag;
