c5a8aae5688b42ec270f7abb01cc47035cdc4acc
Andreas Rissling <a.rissling@steinberg.de>
2022-07-11 16:39:39 +0200

    Add logic to render with children

--- src/common/components/ContactLink/ContactLink.test.tsx
+++ src/common/components/ContactLink/ContactLink.test.tsx
@@ -7,10 +7,26 @@ describe('ContactLink', () => {
   const href = 'www.gueteklasse-a.de';
   const component = <ContactLink href={href}>{text}</ContactLink>;
 
-  it('renders with children', () => {
+  it('renders with one child', () => {
     render(component);
 
     expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  it('renders with children', () => {
+    const componentWithChildren = (
+      <ContactLink href={href}>
+        <div>Child 1</div>
+        <div>Child 2</div>
+      </ContactLink>
+    );
+
+    render(componentWithChildren);
+
+    const link = screen.getByRole('link');
+
+    expect(link.getAttribute('href')).toBe(href);
+    expect(link.innerHTML).toBe('<div>Child 1</div><div>Child 2</div>');
   });
 
   it('renders as an anchor tag', () => {
