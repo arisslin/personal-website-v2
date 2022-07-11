c9909a1a3460488bdc56a6551b5b81b031ad69bd
Andreas Rissling <a.rissling@steinberg.de>
2022-07-11 17:13:08 +0200

    Uninstall email-validator

--- src/common/components/ContactLink/ContactLink.tsx
+++ src/common/components/ContactLink/ContactLink.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
-import * as EmailValidator from 'email-validator';
 
 type ContactLinkProps = {
   children: React.ReactNode;
   href: string;
 };
 
-const ContactLink = ({ children, href }: ContactLinkProps) => {
-  return (
-    <a href={href} rel='noreferrer' target='_blank'>
-      {children}
-    </a>
-  );
-};
+const ContactLink = ({ children, href }: ContactLinkProps) => (
+  <a href={href} rel='noreferrer' target='_blank'>
+    {children}
+  </a>
+);
 
 export default ContactLink;
