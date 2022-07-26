30c6d8fccaefaf11a4e5faddb91f2eea0a725714
Andreas Rissling <a.rissling@steinberg.de>
2022-07-26 13:55:40 +0200

    fix: Dix minify bug in ContactLink on production

--- src/mocks/mockedContactLinks.ts
+++ src/mocks/mockedContactLinks.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { ContactLinkProps } from '../common/components/ContactLink/ContactLink';
 import { FaEnvelope, FaGithub, FaGlobe, FaXing } from 'react-icons/fa';
 
