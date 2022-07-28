8b60d193d443697265351f5f61bb474bc315367a
Andreas Rissling <a.rissling@steinberg.de>
2022-07-28 15:25:07 +0200

    Adjust styling of layout-container

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,7 +6,12 @@ import { mockedContactLinks } from '../mocks/mockedContactLinks';
 const ContactPage = () => (
   <Page title='Kontakt'>
     <h1>Kontakt</h1>
-
+    <p className='layout-container'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit neque quos,
+      repellendus suscipit consequuntur praesentium sequi pariatur adipisci
+      beatae! Veritatis nam dolor dignissimos qui soluta magni assumenda
+      cupiditate quam perferendis?
+    </p>
     <ContactSection links={mockedContactLinks} />
   </Page>
 );
