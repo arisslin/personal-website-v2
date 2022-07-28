d918143ee7a0261d857006a5eba56cbb3b7fbe70
Andreas Rissling <a.rissling@steinberg.de>
2022-07-28 15:33:27 +0200

    Style paragraph in contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components/macro';
 import Page from '../common/components/Page/page';
 import ContactSection from '../common/sections/ContactSection/ContactSection';
 import { mockedContactLinks } from '../mocks/mockedContactLinks';
@@ -6,12 +7,16 @@ import { mockedContactLinks } from '../mocks/mockedContactLinks';
 const ContactPage = () => (
   <Page title='Kontakt'>
     <h1>Kontakt</h1>
-    <p className='layout-container'>
+    <Paragraph className='layout-container'>
       Noch Fragen? Dann schreib mir eine Mail oder besuche mich in den sozialen
       Medien und auf GitHub.
-    </p>
+    </Paragraph>
     <ContactSection links={mockedContactLinks} />
   </Page>
 );
+
+const Paragraph = styled.p`
+  margin-bottom: 2rem;
+`;
 
 export default ContactPage;
