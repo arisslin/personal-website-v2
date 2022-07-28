1b5c14253686ee1022500b5cce410f5da1c752e4
Andreas Rissling <a.rissling@steinberg.de>
2022-07-28 15:17:36 +0200

    Move ContactSection from home to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import Page from '../common/components/Page/page';
+import ContactSection from '../common/sections/ContactSection/ContactSection';
+import { mockedContactLinks } from '../mocks/mockedContactLinks';
 
 const ContactPage = () => (
   <Page title='Kontakt'>
     <h1>Kontakt</h1>
+    <ContactSection links={mockedContactLinks} />
   </Page>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,6 @@ const IndexPage = () => {
         dieser Webseite finden Sie einige Informationen über mich und meine
         Tätigkeit als Entwickler.
       </p>
-      <ContactSection links={mockedContactLinks} />
     </Page>
   );
 };
