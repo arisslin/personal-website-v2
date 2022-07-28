fe88844b39fa1e0cda7c3a4d5ee409700da552ea
Andreas Rissling <a.rissling@steinberg.de>
2022-07-28 15:16:24 +0200

    Add contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Page from '../common/components/Page/page';
+
+const ContactPage = () => (
+  <Page title='Kontakt'>
+    <h1>Kontakt</h1>
+  </Page>
+);
+
+export default ContactPage;
